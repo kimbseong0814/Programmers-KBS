12ccff16375d7c802e970b5c824eb8d7425c5bda
Byungseong <bseong0814@gmail.com>
2026-01-10 18:07:52 +0900

    Update router.js

--- 02. 웹 서비스의 이해/26.01.12/router.js
+++ 02. 웹 서비스의 이해/26.01.12/router.js
@@ -1,15 +1,13 @@
 function route(pathname, handle, response) {
-    console.log('pathname : ' + pathname);
+  console.log('pathname :', pathname);
 
-    if (typeof handle[pathname] == 'function') {
-        handle[pathname] (response);
-    }else {
-        response.writeHead(404, {'Content-Type' : 'text/html'});
-        response.write('찾으시는 페이지가 없습니다.');
-        response.end();  
-    }
-
-    
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response);  
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+    response.write('찾으시는 페이지가 없습니다.');
+    response.end();
+  }
 }
 
 exports.route = route;
