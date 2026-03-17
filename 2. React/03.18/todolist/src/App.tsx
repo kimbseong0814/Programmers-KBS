904f229be5f43fcba14e50fe487c2e6db291c3fe
김병성 <kimbseong0814@gmail.com>
2026-03-17 16:40:37 +0900

    Add React 03.18 files

---
+++ 2. React/03.18/todolist/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name = "리액트";
+
+  // const style = {
+  //   backgroundColor : 'black',
+  //   color : 'white',
+  //   fontsize : '48px',
+  //   fontweight : 'bold',
+  //   padding : '20px'
+  // }
+
+  return (
+    <div className="container">
+      <h1 className = 'test'> Hello, React!!, {
+        name === '리액트' ? (<h1>YES</h1>): null
+        }!!</h1>
+      <p>반갑습니다.</p>
+         
+    </div>
+  );
+}
+
+
+export default App;
